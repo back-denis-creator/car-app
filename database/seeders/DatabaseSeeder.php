0e2f48870e4dcbf318d84023a78b38be54db87ed
Denis B <denis.b@zina.com.ua>
2022-02-19 16:16:51 +0200

    Add seeder faker car

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,14 @@
 <?php
 
-namespace Database\Seeders;
+namespace Database\seeders;
 
+use App\Models\Lists\Country;
+use App\Models\Lists\Fuel;
+use App\Models\Lists\Manufacture;
+use App\Models\Lists\Mark;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +19,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = (new \Faker\Factory())::create();
+        $faker->addProvider(new \Faker\Provider\Fakecar($faker));
+
+        for($i = 0; $i < 100; $i++) {
+            DB::table('marks')->insert([
+                'name' => $faker->vehicleModel,
+            ]);
+        }
+
+        for($i = 0; $i < 50; $i++) {
+            DB::table('manufactures')->insert([
+                'name' => $faker->vehicleBrand,
+            ]);
+        }
+
+        DB::table('fuels')->insert([
+            ['name' => 'бензин'],
+            ['name' => 'газ'],
+            ['name' => 'гибрид'],
+            ['name' => 'электро'],
+            ['name' => 'дизель'],
+        ]);
+        
+        for($i = 0; $i < 100; $i++) {
+            DB::table('countries')->insert([
+                'name' => $faker->country,
+            ]);
+        }
+
+        for($i = 0; $i < 10; $i++) {
+            DB::table('cars')->insert([
+                'picture' => Storage::url('car.png'),
+                'mark_id' => Mark::inRandomOrder()->get()[0]['id'],
+                'manufacturer_id' => Manufacture::inRandomOrder()->get()[0]['id'],
+                'vin' => $faker->vin,
+                'fuel_id' => Fuel::inRandomOrder()->get()[0]['id'],
+                'engine_capacity' => $this->random_float(0, 10),
+                'production_year' => $faker->biasedNumberBetween(1998,2017, 'sqrt'),
+                'price' => $this->random_float(100, 10000),
+                'country_id' => Country::inRandomOrder()->get()[0]['id']
+            ]);
+        }
+        
+    }
+
+    function random_float($min, $max) {
+        return random_int($min, $max - 1) + (random_int(0, PHP_INT_MAX - 1) / PHP_INT_MAX );
     }
 }
